873f40f0f9ee1eb4f394a889fd8065c614190702
中出 恵美 <me-nakade@secom.co.jp>
2025-12-24 17:33:59 +0900

    fix: url修正

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://me-nakade.github.io/sb-todo-list/sfdc_crew_API.yaml",
+    url: "https://me-nakade.github.io/sb-todo-list/sample_API.yaml",
     //url: "https://petstore.swagger.io/v2/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
