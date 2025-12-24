3209c67ee00a05e47412e25ce0a8bcf9537e73db
中出 恵美 <me-nakade@secom.co.jp>
2025-12-24 18:10:24 +0900

    feat: 複数のyamlファイルで検証

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,10 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://me-nakade.github.io/sb-todo-list/sample_API.yaml",
+    urls: [
+      { url:"https://me-nakade.github.io/sb-todo-list/sample_API.yaml", name:"Sample API" },
+      { url:"https://me-nakade.github.io/sb-todo-list/user_mng_API.yaml", name:"User API" }
+    ],
     //url: "https://petstore.swagger.io/v2/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
