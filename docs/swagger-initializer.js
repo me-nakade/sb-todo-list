d7f11b12e3ca374204e8a2d062e9a0f99da94842
中出 恵美 <me-nakade@secom.co.jp>
2026-01-07 15:14:24 +0900

    fix: URL修正

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -4,8 +4,8 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [
-      { url:"https://me-nakade.github.io/sb-todo-list/sample_API.yaml", name:"Sample API" },
-      { url:"https://me-nakade.github.io/sb-todo-list/user_mng_API.yaml", name:"User API" }
+      { url:"https://me-nakade.github.io/sb-todo-list/api_design/sample_API.yaml", name:"Sample API" },
+      { url:"https://me-nakade.github.io/sb-todo-list/api_design/user_mng_API.yaml", name:"User API" }
     ],
     //url: "https://petstore.swagger.io/v2/swagger.json",
     dom_id: '#swagger-ui',
