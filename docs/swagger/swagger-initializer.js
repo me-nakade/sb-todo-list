201c981a036ea009099fd17e90d6faf3fb8fa0e3
中出 恵美 <me-nakade@secom.co.jp>
2026-01-08 17:37:52 +0900

    feat: jsファイル元に戻す

--- docs/swagger/swagger-initializer.js
+++ docs/swagger/swagger-initializer.js
@@ -1,10 +1,26 @@
 window.onload = function() {
-  const ui = SwaggerUIBundle({
-      { url: "sample_API.yaml", name: "サンプルAPI 一覧" },
-      { url: "sample_B_API.yaml", name: "SAMPLE_API" },
-      { url: "user_mng_API.yaml", name: "ユーザー管理API 一覧" },
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    urls: [
+      // AUTO-GENERATED-URLS-START
+      { url:"https://me-nakade.github.io/sb-todo-list/api_design/sample_API.yaml", name:"Sample API" },
+      { url:"https://me-nakade.github.io/sb-todo-list/api_design/user_mng_API.yaml", name:"User API" }
+      // AUTO-GENERATED-URLS-END
     ],
-    dom_id: '#swagger-ui'
+    //url: "https://petstore.swagger.io/v2/swagger.json",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
   });
-  window.ui = ui;
-}
+
+  //</editor-fold>
+};
